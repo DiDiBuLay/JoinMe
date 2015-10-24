37b17f9e0e29e3867373fcafac37fa0411b1830d
WangHsinYuan <dante@sixnology.com>
2015-10-24 22:57:34 +0800

    Add simple azure sdk.

--- JoinMeAndroid/app/src/main/java/com/example/dante/joinme/MapsActivity.java
+++ JoinMeAndroid/app/src/main/java/com/example/dante/joinme/MapsActivity.java
@@ -7,12 +7,18 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import com.microsoft.windowsazure.mobileservices.*;
 
+import java.net.MalformedURLException;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
+    private MobileServiceClient mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +28,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        try {
+            mClient = new MobileServiceClient(
+                    "https://join-me.azure-mobile.net/",
+                    "cNsrvVDiQFZLIvfJXQbChGrKlZrckT34",
+                    this
+            );
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        Marker myMarker = mMap.addMarker(new MarkerOptions().position(sydney).title("19:00~21:00"));
+        myMarker.setSnippet("星巴克買一送一");
+        myMarker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.red_marker));
+        myMarker.showInfoWindow();
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
